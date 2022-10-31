175d31dec0d89d1fea9631cb1feb55912f6e1c45
Uri Shaked <uri@urishaked.com>
2022-10-31 16:34:18 +0200

    chore: configure vite for subdirectory deployment

---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '', // this allows deployment to a subdirectory (e.g. https://tinytapeout.github.io/tinytapeout_gds_viewer/)
+});
